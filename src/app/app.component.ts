53cf4301883dbb04afb901c0989d1d87dc8ff935
THE-AUDIO <nandraina.dev22@gmail.com>
2025-05-16 08:12:41 +0300

    toogle menu anim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
 
   title = 'M&L';
-  isDarkMode = true;
-
-    toogleMode(){
+  isDarkMode = false;
+  isHidden = false;
+    toggleMode(){
       this.isDarkMode =!this.isDarkMode
       console.log(this.isDarkMode);
       
@@ -21,6 +22,12 @@ export class AppComponent implements OnInit{
         document.body.classList.remove('dark')
       }
     }  
+    toggleMenu(){
+      this.isHidden =!this.isHidden
+      console.log(this.isHidden);
+      
+    }
+
 
     ngOnInit(): void {
   }
